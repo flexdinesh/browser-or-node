b39d40a0318892b7a352776678cf021ceb594d9f
Dinesh Pandiyan <flexdinesh@gmail.com>
2020-06-25 13:44:02 +1000

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 /* global window self */
 
 const isBrowser =
-  typeof window !== "undefined" && typeof window.document !== "undefined";
+  typeof window !== 'undefined' && typeof window.document !== 'undefined';
 
 /* eslint-disable no-restricted-globals */
 const isWebWorker =
-  typeof self === "object" &&
+  typeof self === 'object' &&
   self.constructor &&
-  self.constructor.name === "DedicatedWorkerGlobalScope";
+  self.constructor.name === 'DedicatedWorkerGlobalScope';
 /* eslint-enable no-restricted-globals */
 
 const isNode =
-  typeof process !== "undefined" &&
+  typeof process !== 'undefined' &&
   process.versions != null &&
   process.versions.node != null;
 
@@ -19,9 +19,10 @@ const isNode =
  * @see https://github.com/jsdom/jsdom/releases/tag/12.0.0
  * @see https://github.com/jsdom/jsdom/issues/1537
  */
+/* eslint-disable no-undef */
 const isJsDom = () =>
-  (typeof window !== "undefined" && window.name === "nodejs") ||
-  navigator.userAgent.includes("Node.js") ||
-  navigator.userAgent.includes("jsdom");
+  (typeof window !== 'undefined' && window.name === 'nodejs') ||
+  navigator.userAgent.includes('Node.js') ||
+  navigator.userAgent.includes('jsdom');
 
 export { isBrowser, isWebWorker, isNode, isJsDom };
