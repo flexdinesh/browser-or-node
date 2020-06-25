024de0da202b3e53e01923553696a4fa49ef0961
Rohan Deshpande <rdeshpande@atlassian.com>
2020-06-25 13:37:57 +1000

    Added isJsDom function to check for jsdom environments

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 /* global window self */
 
-const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+const isBrowser =
+  typeof window !== "undefined" && typeof window.document !== "undefined";
 
 /* eslint-disable no-restricted-globals */
 const isWebWorker =
-  typeof self === 'object' &&
+  typeof self === "object" &&
   self.constructor &&
-  self.constructor.name === 'DedicatedWorkerGlobalScope';
+  self.constructor.name === "DedicatedWorkerGlobalScope";
 /* eslint-enable no-restricted-globals */
 
 const isNode =
-  typeof process !== 'undefined' &&
+  typeof process !== "undefined" &&
   process.versions != null &&
   process.versions.node != null;
 
-export {
-  isBrowser,
-  isWebWorker,
-  isNode
-};
+/**
+ * @see https://github.com/jsdom/jsdom/releases/tag/12.0.0
+ * @see https://github.com/jsdom/jsdom/issues/1537
+ */
+const isJsDom = () =>
+  (typeof window !== "undefined" && window.name === "nodejs") ||
+  navigator.userAgent.includes("Node.js") ||
+  navigator.userAgent.includes("jsdom");
+
+export { isBrowser, isWebWorker, isNode, isJsDom };
