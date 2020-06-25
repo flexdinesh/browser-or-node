545bf1649148fff3006127e0c8fea3d0bf488f1a
Dinesh Pandiyan <flexdinesh@gmail.com>
2020-06-25 13:56:51 +1000

    Upgrade deps

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 /* global window self */
 
-const isBrowser =
-  typeof window !== 'undefined' && typeof window.document !== 'undefined';
+const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
 
 /* eslint-disable no-restricted-globals */
-const isWebWorker =
-  typeof self === 'object' &&
-  self.constructor &&
-  self.constructor.name === 'DedicatedWorkerGlobalScope';
+const isWebWorker = typeof self === 'object'
+  && self.constructor
+  && self.constructor.name === 'DedicatedWorkerGlobalScope';
 /* eslint-enable no-restricted-globals */
 
-const isNode =
-  typeof process !== 'undefined' &&
-  process.versions != null &&
-  process.versions.node != null;
+const isNode = typeof process !== 'undefined'
+  && process.versions != null
+  && process.versions.node != null;
 
 /**
  * @see https://github.com/jsdom/jsdom/releases/tag/12.0.0
  * @see https://github.com/jsdom/jsdom/issues/1537
  */
 /* eslint-disable no-undef */
-const isJsDom = () =>
-  (typeof window !== 'undefined' && window.name === 'nodejs') ||
-  navigator.userAgent.includes('Node.js') ||
-  navigator.userAgent.includes('jsdom');
+const isJsDom = () => (typeof window !== 'undefined' && window.name === 'nodejs')
+  || navigator.userAgent.includes('Node.js')
+  || navigator.userAgent.includes('jsdom');
 
-export { isBrowser, isWebWorker, isNode, isJsDom };
+export {
+  isBrowser, isWebWorker, isNode, isJsDom
+};
