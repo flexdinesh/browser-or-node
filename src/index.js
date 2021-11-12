1190c8fd9d972acb45f448f0418bae5aac5e2b3f
Dinesh Pandiyan <flexdinesh@gmail.com>
2021-11-13 09:52:21 +1100

    Remove airbng config and use prettier config

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-/* global window self */
+const isBrowser =
+  typeof window !== "undefined" && typeof window.document !== "undefined";
 
-const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+const isWebWorker =
+  typeof self === "object" &&
+  self.constructor &&
+  self.constructor.name === "DedicatedWorkerGlobalScope";
 
-/* eslint-disable no-restricted-globals */
-const isWebWorker = typeof self === 'object'
-  && self.constructor
-  && self.constructor.name === 'DedicatedWorkerGlobalScope';
-/* eslint-enable no-restricted-globals */
-
-const isNode = typeof process !== 'undefined'
-  && process.versions != null
-  && process.versions.node != null;
+const isNode =
+  typeof process !== "undefined" &&
+  process.versions != null &&
+  process.versions.node != null;
 
 /**
  * @see https://github.com/jsdom/jsdom/releases/tag/12.0.0
  * @see https://github.com/jsdom/jsdom/issues/1537
  */
-/* eslint-disable no-undef */
-const isJsDom = (() => (typeof window !== 'undefined' && window.name === 'nodejs')
-  || navigator.userAgent.includes('Node.js')
-  || navigator.userAgent.includes('jsdom'))();
+const isJsDom = (() =>
+  (typeof window !== "undefined" && window.name === "nodejs") ||
+  navigator.userAgent.includes("Node.js") ||
+  navigator.userAgent.includes("jsdom"))();
 
-const isDeno = typeof Deno !== 'undefined' && typeof Deno.core !== 'undefined';
+const isDeno = typeof Deno !== "undefined" && typeof Deno.core !== "undefined";
 
-export {
-  isBrowser, isWebWorker, isNode, isJsDom, isDeno
-};
+export { isBrowser, isWebWorker, isNode, isJsDom, isDeno };
--- test/index.js
+++ test/index.js
@@ -1,23 +1,23 @@
-import { assert } from 'chai';
-import { isBrowser, isWebBrowser, isNode, isDeno, isJsDom } from '../src';
+import { assert } from "chai";
+import { isBrowser, isWebBrowser, isNode, isDeno, isJsDom } from "../src";
 
-console.log('Browser?', isBrowser);
-console.log('Node?', isNode);
-console.log('Web browser?', isWebBrowser);
-console.log('JS Dom?', isJsDom);
-console.log('Deno?', isDeno);
+console.log("Browser?", isBrowser);
+console.log("Node?", isNode);
+console.log("Web browser?", isWebBrowser);
+console.log("JS Dom?", isJsDom);
+console.log("Deno?", isDeno);
 
-describe('Browser or Node.js', () => {
-  it('should check node env', () => {
-    assert(isNode === true, 'isNode didn\'t work :(');
+describe("Browser or Node.js", () => {
+  it("should check node env", () => {
+    assert(isNode === true, "isNode didn't work :(");
   });
 
-  it('should check browser env', () => {
+  it("should check browser env", () => {
     //should figure out how to automate this
-    assert(isBrowser === false, 'isBrowser didn\'t work :(');
+    assert(isBrowser === false, "isBrowser didn't work :(");
   });
 
-  it('should check web worker env', () => {
-    assert(isNode === true, 'isWebWorker didn\'t work :(');
+  it("should check web worker env", () => {
+    assert(isNode === true, "isWebWorker didn't work :(");
   });
 });
