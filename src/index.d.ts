adee19e1ad6499557c33120069fd5cfb00bc9374
Dinesh Pandiyan <flexdinesh@gmail.com>
2021-11-13 10:00:04 +1100

    Add isJsDom and isDeno types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,5 @@
 export declare const isBrowser: boolean;
 export declare const isWebWorker: boolean;
 export declare const isNode: boolean;
+export declare const isJsDom: boolean;
+export declare const isDeno: boolean;
