d0ffc364858e490e4dfce4b86104fe0879656aea
Dinesh Pandiyan <flexdinesh@gmail.com>
2023-11-19 14:09:56 +1100

    Migrate build from babel to tsup

--- examples/example.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies, no-console */
-const jsEnv = require('../lib');
-
-console.log(jsEnv.isBrowser); // false when this runs in node.js
-console.log(jsEnv.isNode); // true when this runs in node.js
