130523707ea8443f4ac08cd193277b178ae6f7ee
xeho91 <xeho91@pm.me>
2024-02-07 16:34:44 +0800

    Remove obsolete tests

--- test/bun.ts
+++
@@ -1,5 +0,0 @@
-// Special check for isBun
-// Run with `Bun run test/bun.ts`
-import { isBun } from "../src/index.js";
-
-console.log({ isBun });
--- test/index.ts
+++
@@ -1,5 +0,0 @@
-// Special check for isDeno
-// Run with `deno run test/index.ts`
-import { isDeno } from "../src/index.js";
-
-console.log({ isDeno });
